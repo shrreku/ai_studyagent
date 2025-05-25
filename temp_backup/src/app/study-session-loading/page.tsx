c4aaf9d1481a22ce144eb59f2d0ac1999813a4a2
Shreyash Kumar <kshreyash12345@gmail.com>
2025-05-26 03:28:18 +0530

    Update .gitignore and remove test data, docs, and temp_backup from git tracking

--- temp_backup/src/app/study-session-loading/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import StudySessionLoading from '@/components/loading/StudySessionLoading';
-
-export default function StudySessionLoadingPage() {
-  return <StudySessionLoading />;
-}
